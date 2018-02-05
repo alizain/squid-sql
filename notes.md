9cd351b345144a9bbbb63b5730a67382697660ab
Alizain Feerasta <alizain.feerasta@gmail.com>
2018-02-05 11:33:39 -0500

    Initial commit

---
+++ notes.md
@@ -0,0 +1,18 @@
+# Notes
+
+The basic structure of this implementation is:
+  1. run the `from` queries, to get the required tables
+  2. filter the tables according to the `where` queries
+  3. `select` the right columns from the filtered tables and output
+
+The core construct is the `Table` class. When a CSV is first loaded, it is initialized into a `Table`. However, `Tables` or not really tied one-to-one for each CSV file. It's easier to think about the `Table` as a `TableView`; it could contain columns from many tables. The class itself keeps track of which columns belong to which table name for the duration of the query. This makes it really easy to run `where` filters on joined tables, because the fact that it is a joined table is abstracted away. Even things like `as` become simple to handle, as you'll see in the code.
+
+`ColumnConfig` is another important component of the app, and is the main reason why a Table can contain columns from many different tables. The `ColumnConfig` contains everything needed to determine which columns are from which table. Because the data-structure is immutable, we can use `_replace()` functions to easily handle `as` queries, as mentioned above.
+
+At the moment, because this was a 3 hour exercise, we basically join all tables into one mega table before running any `where` or `select` queries. What this means is that if you `select` a table with `Table(N)` rows, and another table with `Table(M)` rows, you end up with a table that is `Table(M * N)` rows. If you `select` a third table with `Q` rows, you end up with a table that is `Table(M * N * Q)` rows. This shortcut makes the current implementation unbelievably straight-forward, but obviously very inefficient.
+
+Steps to make this implementation more efficient:
+  - `Where` queries can be classified as `ColumnToLiteralWhere` queries or `ColumnToColumnWhere`. `ColumnToLiteralWhere` only require 1 table without any joins. So the first thing I would do is to run the `ColumnToLiteralWhere` queries on the original tables, before merging them. That should reduce the problem space significantly, to `Table(N' * M')`.
+  - Now when you have `N' & M'`, you can either do the work in two passes, where the first pass is merging the two tables `Table(N' * M')`, and the second pass is running the filter, or you can do both in one pass. If there are no `limit` queries, than doing it in one pass will reduce work by 2x in the best case (1 loop instead of 2). If there are `limit` clauses, you can significantly reduce the amount of work you have to do, because you simply don't need to visit any more rows after you have the number you need.
+  - Which leads me to the next and interesting implementation of a SQL engine - lazy evaluation. Essentially, you wouldn't want to do any work, till you absolutely need to. So in Python, I'd take all the applicable `ColumnToLiteralWhere` functions, and bind them to `Table(N)`, which would return a lazily-evaluating generator that can yield rows. This generator can be used by a `TableToTable` class to handle `ColumnToColumnWhere` queries, which itself is a generator, that can be used by the `select` query engine. So as the client requests a new row to display, the various connected generators yield a new row that matches all the required queries. If I had the time, this would be a fun implementation to write.
+  - At some point, I'd move the querying/filtering logic into their own classes, that essentially can take as input one or more tables, a series of where queries, and optional indexes, and then return a generator that yields matching rows as required.
---
+++ v1.py
@@ -0,0 +1,364 @@
+#! /usr/bin/env python3
+
+import sys
+import json
+from collections import namedtuple
+
+
+major, minor = sys.version_info[:2]
+if not (major == 3 and minor >= 5):
+    curr_version = '.'.join(map(str, sys.version_info[:3]))
+    sys.stderr.write(f'Please use Python 3.5+ for this program.\nYou\'re trying to run it with Python {curr_version}.\n')
+    sys.exit(1)
+
+
+class SquidError(Exception):
+    component = ''
+    def __init__(self, message, *args, **kwargs):
+        super().__init__(message, *args, **kwargs)
+        self.message = message
+
+class SquidCLIError(SquidError):
+    component = 'CLI'
+
+
+SUPPORTED_COLUMN_TYPES = ['int', 'str']
+SUPPORTED_OPS_BY_COLUMN_TYPE = {
+    'int': ['=', '!=', '>', '>=', '<', '<='],
+    'str': ['=', '!='],
+}
+
+
+def equal_to(left, right):
+    return left == right
+
+def not_equal_to(left, right):
+    return left != right
+
+def greater_than(left, right):
+    return left > right
+
+def greater_than_or_equal_to(left, right):
+    return greater_than(left, right) or equal_to(left, right)
+
+def less_than(left, right):
+    return left < right
+
+def less_than_or_equal_to(left, right):
+    return less_than(left, right) or equal_to(left, right)
+
+OPERATOR_FUNCS = {
+    '=': equal_to,
+    '!=': not_equal_to,
+    '>': greater_than,
+    '>=': greater_than_or_equal_to,
+    '<': less_than,
+    '<=': less_than_or_equal_to
+}
+
+
+ColumnConfig = namedtuple('ColumnConfig', ['table', 'name', 'type', 'index'])
+
+
+class TableLoader:
+
+    def __init__(self, table_class, filename_extra='.table.json'):
+        self.table_class = table_class
+        self.filename_extra = filename_extra
+        self.table_cache = {}
+
+    def name_to_filename(self, name):
+        return f'{name}{self.filename_extra}'
+
+    def load_table_from_file(self, table_name):
+        filename = self.name_to_filename(table_name)
+        try:
+            with open(filename, 'r') as raw_data:
+                raw_rows = json.load(raw_data)
+                num_rows = len(raw_rows) - 1
+                print(f'- Loaded "{filename}", {num_rows} rows.')
+                table = self.table_class.from_file(table_name, raw_rows)
+                self.table_cache[table_name] = table
+                return table
+        except FileNotFoundError:
+            raise SquidError(f'table:{table_name} does not exist')
+
+    def load_or_get_table(self, name):
+        try:
+            return self.table_cache[name]
+        except KeyError:
+            return self.load_table_from_file(name)
+
+
+class Table:
+
+    def __init__(self, columns, rows):
+        self.columns = tuple(columns)
+        self.rows = tuple(rows)
+
+    def col_config_by_column_ref(self, column_ref):
+        found_columns = []
+        for col_config in self.columns:
+            conditions = []
+            if column_ref['table']:
+                conditions.append(col_config.table == column_ref['table'])
+            conditions.append(col_config.name == column_ref['name'])
+            if all(conditions):
+                found_columns.append(col_config)
+        if len(found_columns) < 1:
+            raise SquidError(f'column_ref:{column_ref} not found, maybe you haven\'t seleced the required table')
+        if len(found_columns) > 1:
+            found_table_names = '|'.join(tuple(col_config.table for col_config in found_columns))
+            raise SquidError(f'column_ref:{column_ref} is ambigious, found multiple matching columns in tables:{found_table_names}')
+        return found_columns[0]
+
+    def apply_where_query(self, where_query):
+        if isinstance(where_query, ColumnToLiteralWhere):
+            return self.apply_column_to_literal_where_query(where_query)
+        elif isinstance(where_query, ColumnToColumnWhere):
+            return self.apply_column_to_column_where_query(where_query)
+        else:
+            raise SquidError(f'unknown where query type:{where_query.__class__}')
+
+    def apply_column_to_column_where_query(self, where_query):
+        left_col = self.col_config_by_column_ref(where_query.left['column'])
+        right_col = self.col_config_by_column_ref(where_query.right['column'])
+        if left_col.type != right_col.type:
+            raise SquidError(f'cannot compare column:{left_col.name} of type:{left_col.type} with column:{right_col.name} of type:{right_col.type}')
+        if where_query.op not in SUPPORTED_OPS_BY_COLUMN_TYPE[left_col.type]:
+            raise SquidError(f'non-sensical operator:{where_query.op} for type:{left_col.type}')
+        operator_func = OPERATOR_FUNCS[where_query.op]
+        new_rows = []
+        for row in self.rows:
+            if operator_func(row[left_col.index], row[right_col.index]):
+                new_rows.append(row)
+        return self.__class__(self.columns, new_rows)
+
+    def apply_column_to_literal_where_query(self, where_query):
+        col_config = self.col_config_by_column_ref(where_query.column_term['column'])
+        literal_value_key = f'lit_{col_config.type}'
+        if literal_value_key not in where_query.literal_term:
+            raise SquidError(f'cannot compare column:{col_config.name} of type:{col_config.type} with term:{where_query.literal_term}')
+        if where_query.op not in SUPPORTED_OPS_BY_COLUMN_TYPE[col_config.type]:
+            raise SquidError(f'non-sensical operator:{where_query.op} for type:{col_config.type}')
+        operator_func = OPERATOR_FUNCS[where_query.op]
+        literal_value = where_query.literal_term[literal_value_key]
+        new_rows = []
+        if where_query.column_side == 'left':
+            for row in self.rows:
+                if operator_func(row[col_config.index], literal_value):
+                    new_rows.append(row)
+        elif where_query.column_side == 'right':
+            for row in self.rows:
+                if operator_func(literal_value, row[col_config.index]):
+                    new_rows.append(row)
+        else:
+            raise Exception('this should never happen because we\'ve already '
+                            'validated the `where` query when we created the '
+                            'ColumnToLiteralWhere class. If you\'ve arrived at '
+                            'this error, you haven\'t passed in a real '
+                            'ColumnToLiteralWhere, or you\'ve done some other '
+                            'really bad AbuseOfDynamicTyping™ thing!')
+        return self.__class__(self.columns, new_rows)
+
+    def apply_as_exp(self, as_name):
+        new_columns = tuple(col_config._replace(table=as_name) for col_config in self.columns)
+        return self.__class__(new_columns, self.rows)
+
+    def apply_select_queries(self, raw_select_queries):
+        found_columns = []
+        found_column_names = []
+        for select_query in raw_select_queries:
+            col_config = self.col_config_by_column_ref(select_query['source']['column'])
+            col_name = col_config.name
+            if select_query['as']:
+                col_name = select_query['as']
+            if col_name in found_column_names:
+                raise SquidError(f'column as:{col_name} already `select`ed in this query, please use a unique name')
+            col_config = col_config._replace(name=col_name)
+            found_column_names.append(col_name)
+            found_columns.append(col_config)
+        new_rows = self.filter_rows_for_columns(found_columns)
+        new_columns = tuple(col_config._replace(index=index) for index, col_config in enumerate(found_columns))
+        return self.__class__(new_columns, new_rows)
+
+    def filter_rows_for_columns(self, columns_to_include):
+        new_rows = []
+        for existing_row in self.rows:
+            row = []
+            for col in columns_to_include:
+                val = existing_row[col.index]
+                row.append(val)
+            new_rows.append(row)
+        return new_rows
+
+    def printable_rows_with_column_headers(self):
+        columns = tuple(col.name for col in self.columns)
+        return (columns, self.rows)
+
+    @classmethod
+    def merge_tables(cls, one, two):
+        to_add = len(one.columns)
+        two_columns_fixed = tuple(col._replace(index=col.index + to_add) for col in two.columns)
+        new_columns = one.columns + two_columns_fixed
+        new_rows = []
+        for one_row in one.rows:
+            for two_row in two.rows:
+                new_rows.append(one_row + two_row)
+        return cls(new_columns, new_rows)
+
+    @classmethod
+    def from_file(cls, table_name, raw_rows):
+        raw_columns = raw_rows.pop(0)
+        columns = cls.validate_and_construct_col_configs(table_name, raw_columns)
+        rows = tuple(raw_rows)
+        return cls(columns, rows)
+
+    @staticmethod
+    def validate_and_construct_col_configs(table_name, raw_columns):
+        columns = []
+        for index, [col_name, col_type] in enumerate(raw_columns):
+            col_config = ColumnConfig(name=col_name, type=col_type, table=table_name, index=index)
+            columns.append(col_config)
+        return tuple(columns)
+
+
+class ColumnToLiteralWhere(namedtuple('ColumnToLiteralWhere', [
+    'op',
+    'left',
+    'right',
+    'column_side',
+    'literal_side'
+])):
+
+    def __new__(cls, op, left, right):
+        if 'column' in left and 'column' in right:
+            raise SquidError('this class may be what you want, but it\'s certainly '
+                             'not what you need, becasue what you gave me is not a '
+                             'column-to-literal where query - it\'s actually a '
+                             'column-to-column where query.')
+        elif 'column' in left and 'column' not in 'right':
+            return super().__new__(cls, op, left, right, column_side='left', literal_side='right')
+        elif 'column' not in left and 'column' in 'right':
+            return super().__new__(cls, op, left, right, column_side='right', literal_side='left')
+        else:
+            raise SquidError('malformed where expression, you doing okay?')
+
+    @property
+    def column_term(self):
+        return getattr(self, self.column_side)
+
+    @property
+    def literal_term(self):
+        return getattr(self, self.literal_side)
+
+
+class ColumnToColumnWhere(namedtuple('ColumnToColumnWhere', [
+    'op',
+    'left',
+    'right'
+])):
+
+    pass
+
+
+def run_query(table_loader, raw_query):
+    raw_from_queries = raw_query['from']
+    raw_select_queries = raw_query['select']
+    raw_where_queries = raw_query.get('where', [])
+
+    tables = []
+
+    # run `from` queries
+    used_table_names = []
+    for from_query in raw_from_queries:
+        source_table_name = from_query['source']['file']
+        new_table = table_loader.load_or_get_table(source_table_name)
+        new_table_name = from_query['as'] or source_table_name
+        if new_table_name in used_table_names:
+            raise SquidError(f'table:{new_table_name} has already been declared in this query, try using `as` to give it a distinct name')
+        if new_table_name != source_table_name:
+            new_table = new_table.apply_as_exp(from_query['as'])
+        used_table_names.append(new_table_name)
+        tables.append(new_table)
+
+    # do the shortcut merge upfront
+    query_table = tables.pop(0)
+    while tables:
+        table_to_merge = tables.pop(0)
+        query_table = Table.merge_tables(query_table, table_to_merge)
+
+    # run `where` queries
+    if raw_where_queries:
+        all_where_queries = build_where_queries(raw_where_queries)
+        for where_query in all_where_queries:
+            query_table = query_table.apply_where_query(where_query)
+
+    query_table = query_table.apply_select_queries(raw_select_queries)
+    pretty_print_table(query_table)
+
+
+def build_where_queries(raw_where_queries):
+    built_queries = []
+    for where_query in raw_where_queries:
+        if 'column' in where_query['left'] and 'column' in where_query['right']:
+            built_queries.append(ColumnToColumnWhere(**where_query))
+        else:
+            built_queries.append(ColumnToLiteralWhere(**where_query))
+    return built_queries
+
+
+def pretty_print_table(query_table):
+    columns, all_rows = query_table.printable_rows_with_column_headers()
+    col_divider = ' | '
+    col_max_widths = find_col_max_widths((columns,) + all_rows)
+    col_formatters = create_col_formatters(col_max_widths)
+    max_width = sum(col_max_widths) + ((len(col_max_widths) - 1) * len(col_divider))
+    str_row_divider = '-' * max_width
+    str_columns = stringify_row(col_divider, col_formatters, columns)
+    str_rows = tuple(stringify_row(col_divider, col_formatters, row) for row in all_rows)
+    print('\n'.join((str_columns, str_row_divider) + str_rows))
+
+def stringify_row(divider, formatters, row):
+    row_stringified = (formatter.format(value) for formatter, value in zip(formatters, row))
+    return divider.join(row_stringified)
+
+def create_col_formatters(col_max_widths):
+    all_but_last = tuple(f'{{:<{max_width}}}' for max_width in col_max_widths[:-1])
+    last = col_max_widths[-1]
+    return all_but_last + (f'{{:>{last}}}',)
+
+def find_col_max_widths(all_rows):
+    num_columns = len(all_rows[0])
+    col_max_widths = [0] * num_columns
+    for row in all_rows:
+        for index in range(num_columns):
+            curr_len = len(str(row[index]))
+            if curr_len > col_max_widths[index]:
+                col_max_widths[index] = curr_len
+    return col_max_widths
+
+
+def parse_query_file_and_run_query(filename):
+    try:
+        with open(filename) as qf:
+            table_loader = TableLoader(table_class=Table)
+            curr_query = json.load(qf)
+            run_query(table_loader, curr_query)
+    except FileNotFoundError:
+        raise SquidError(f'query file:{filename} not found, please make sure it exists')
+
+
+if __name__ == '__main__':
+    try:
+        query_file_name = ''
+        try:
+            query_file_name = sys.argv[1]
+        except IndexError:
+            raise SquidCLIError('no query file provided')
+        parse_query_file_and_run_query(query_file_name)
+    except SquidError as err:
+        if err.component:
+            print(f'ERROR({err.component}): {err.message}')
+        else:
+            print(f'ERROR: {err.message}')
